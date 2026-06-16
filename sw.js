195dd252202fc8448092c2ca0549256b30cd55f1
andrewblyskosz <andrewblyskosz@gmail.com>
2026-06-16 15:49:38 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,19 +1,27 @@
 // Service Worker for Job Tracker PWA
-const CACHE = 'job-tracker-v1';
+const CACHE = 'job-tracker-v2';
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(['/jobtracker/']))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
+  // Network-first: always try to get the freshest version, fall back to cache offline
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
